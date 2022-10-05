aed67b8764f806910c9e25d5d2b457f4ee99fbee
Ms-Waiyego <waiyegobrenda99@gmail.com>
2022-10-06 01:40:15 +0300

    about set-up

---
+++ emoji-search-app/src/Components/About.js
@@ -0,0 +1,11 @@
+function About(){
+    return (
+     <div >
+         <h2 >
+            Welcome and tell us how you feel using an emoji!!!
+         </h2>
+     </div>
+    )
+ }
+ 
+ export default About;
