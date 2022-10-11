a0a4352642c24e85411f012dd9b37ec02ad343df
Ms-Waiyego <waiyegobrenda99@gmail.com>
2022-10-12 02:16:01 +0300

    more styling

--- emoji-search-app/src/Components/Emoji.js
+++ emoji-search-app/src/Components/Emoji.js
@@ -3,11 +3,11 @@ import React from 'react'
 
 const Emoji = ( {emojis}) => {
 return (
- <>
+ <div className='emoji-container'>
  {emojis.map((emoji, index)=>(
   <h4 key={index}>{emoji.symbol} {emoji.title}</h4>
  ))}
- </>
+ </div>
 )
 }
 export default Emoji
