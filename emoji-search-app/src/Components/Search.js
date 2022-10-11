7d6641f31b7f308b1aeea4567d7ea4954e1e741d
Ms-Waiyego <waiyegobrenda99@gmail.com>
2022-10-12 01:42:21 +0300

    changes on search

--- emoji-search-app/src/Components/Search.js
+++ emoji-search-app/src/Components/Search.js
@@ -1,9 +1,7 @@
 import { useState } from "react";
-
 const Form  =({ setValueEmoji }) => {
   const [value, setValue] = useState('');
   const [error, setError] = useState(false);
-
   const handleSearch = e => {
       e.preventDefault();
       if(value === ''){
@@ -17,13 +15,12 @@ const Form  =({ setValueEmoji }) => {
   const allEmojis = () => {
       setValueEmoji('');
   }
-
   return (
     <div className="searchbar">
           <form onSubmit={handleSearch}>
-              <input 
-                  type="text" 
-                  placeholder="emoji word..." 
+              <input
+                  type="text"
+                  placeholder="emoji word..."
                   onChange={e => setValue(e.target.value)} />
          </form>
          <button
@@ -34,5 +31,4 @@ const Form  =({ setValueEmoji }) => {
                 </button>
          </div>
   )}
-
 export default Form
